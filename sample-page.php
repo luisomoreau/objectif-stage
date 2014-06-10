d6d2d3a19e4632c01c1fa0e7d463f4ab43127eb0
lmoreau <luis.omoreau@gmail.com>
2014-06-10 16:25:31 +1100

    add the scss files to use with compass

---
+++ sample-page.php
@@ -0,0 +1,26 @@
+<?php
+$loginreq=0;
+require('settings.php');
+require('logincheck.php');
+require('fonctions.php');
+?>
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Stages UNC</title>
+    <link rel="stylesheet" href="scss/custom.css" />
+    <script src="js/vendor/modernizr.js"></script>
+</head>
+
+<body>
+<div class="row">
+    <div class="large-12 columns">
+        <h1 class="main-title">Objectif stages</h1>
+    </div>
+</div>
+
+
+</body>
+</html>
