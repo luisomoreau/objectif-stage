30c0217ce217d24e1cafab1f920e12690821f699
Numerinico <nicolas.brengard@gmail.com>
2014-06-16 09:37:57 +1100

    tet css

---
+++ test.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Foundation | Welcome</title>
+    <link rel="stylesheet" href="scss/custom.css"/>
+    <script src="js/vendor/custom.modernizr.js"></script>
+</head>
+<body>
+
+
+<!--.page -->
+<div role="document" class="page">
+
+    <!--.l-header region -->
+    <header role="banner" class="l-header">
+
+        <div class="header-top hide-for-small">
+            <div class="row">
+                <div class="large-6 columns header_top_left">
+                    <ul class="social-icons accent inline-list">
+
+                    </ul>
+                </div>
+                <div class="large-6 columns header_top_right text-right">
+                </div>
+            </div>
+        </div>
+
+        <!--.top-bar -->
+        <div class="contain-to-grid sticky">
+            <nav class="top-bar" data-options="">
+                <ul class="title-area">
+                    <li class="name"><h1><a href="/" rel="home" title="&quot;Espace Numérique de Travail&quot;  Accueil"
+                                            class="active">&quot;Espace Numérique de Travail&quot; </a></h1></li>
+                    <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+                </ul>
+                <section class="top-bar-section">
+                    <ul id="main-menu" class="main-nav">
+                        <li class="first leaf color-1"><a href="/" class="has-icon active"><i class="fa fa-home"></i>Accueil</a>
+                        </li>
+                        <li class="expanded color-2 has-dropdown" title=""><a href="/etudiant" title=""
+                                                                              class="has-icon"><i
+                                    class="fa fa-book"></i>Espace étudiant</a>
+                            <ul class="dropdown">
+                                <li class="expanded color-2 show-for-small"><a href="/etudiant" title="">Espace
+                                        étudiant</a>
+                                <li class="first leaf" target="_blank"><a
+                                        href="http://lee.univ-nc.nc/cgi-bin/WebObjects/ipWeb.woa"
+                                        target="_blank">Notes</a></li>
+                                <li class="last leaf" target="_blank"><a href="http://tic.univ-nc.nc/" target="_blank">Campus
+                                        virtuel</a></li>
+                            </ul>
+                        </li>
+                        <li class="leaf color-3"><a href="/messagerie" class="has-icon"><i class="fa fa-envelope"></i>Messagerie</a>
+                        </li>
+                        <li class="last leaf color-4"><a href="/tuto" class="has-icon"><i class="fa fa-lightbulb-o"></i>Tuto</a>
+                        </li>
+                    </ul>
+                </section>
+            </nav>
+        </div>
+    </header>
+    <section class="titlebar ">
+        <div class="row">
+            <div class="large-12 columns">
+                <h1 id="page-title" class="title">Accueil</h1>
+
+            </div>
+        </div>
+    </section>
+</div>
+<script src="js/vendor/jquery.js"></script>
+<script src="js/foundation.min.js"></script>
+<script>
+    $(document).foundation();
+</script>
+</body>
+</html>
