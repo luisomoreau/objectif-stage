1beb5039417032a0dd750081a7af7530966ddbd7
Numerinico <nicolas.brengard@gmail.com>
2014-06-16 11:30:34 +1100

    fichier type créé

--- test.php
+++ test.php
@@ -34,7 +34,7 @@
             <nav class="top-bar" data-options="">
                 <ul class="title-area">
                     <li class="name"><h1><a href="/" rel="home" title="&quot;Espace Numérique de Travail&quot;  Accueil"
-                                            class="active">&quot;Espace Numérique de Travail&quot; </a></h1></li>
+                                            class="active">Objectif Stage !</a></h1></li>
                     <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
                 </ul>
                 <section class="top-bar-section">
@@ -79,11 +79,32 @@
         </div>
     </section>
     <section class="row">
-        <p>
-            Pour commencer connectez vous !
-        </p>
+        <div class="large-6 column">
+            <a href="#" class="large button expand">Vous êtes un étudiant</a>
+        </div>
+        <div class="large-6 column">
+            <a href="#" class="large button expand">Vous êtes une entreprise</a>
+        </div>
     </section>
-
+    <section class="row">
+        <div class="large-12">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc congue sem lorem, id elementum libero fringilla sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at suscipit purus. Etiam ut eleifend velit. Morbi molestie, leo ac condimentum tempus, neque ipsum fringilla neque, nec ultrices libero ante vel est. Donec purus tellus, suscipit sit amet erat eget, posuere auctor turpis. Nam aliquam, turpis ac adipiscing placerat, nibh enim posuere nulla, ac molestie orci erat vitae elit. Integer at tellus nec nunc euismod blandit. Sed imperdiet ac sapien lobortis suscipit. Nullam vehicula et erat ac mollis. Etiam nec consectetur orci, nec hendrerit ante. Nullam auctor at odio in convallis. Proin accumsan nibh sit amet nisi vulputate, ac sodales tortor mattis.
+            </p>
+            <p>
+                Sed imperdiet, turpis et consequat commodo, metus ipsum lacinia odio, et scelerisque elit libero vitae lacus. Aenean bibendum purus vitae tempor iaculis. Fusce auctor tristique arcu, consectetur malesuada erat pulvinar et. Nulla eget rhoncus dui, ac ornare urna. Etiam gravida tristique diam at luctus. Curabitur mattis, justo vel sollicitudin facilisis, lectus mi vulputate augue, non commodo ipsum nibh nec nisi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla eget nisl quis nisi luctus condimentum. Aenean nec ligula turpis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus convallis vel dui in semper.
+            </p>
+            <p>
+                Vivamus auctor urna ut laoreet scelerisque. Quisque ac nibh enim. Aliquam vel consequat dolor, condimentum ultrices est. Duis viverra turpis risus. Phasellus et diam nisl. Morbi posuere laoreet sapien, ac lobortis neque pulvinar nec. In hac habitasse platea dictumst. Integer ut neque vel leo commodo consequat vitae eget dolor. Etiam sit amet turpis adipiscing nulla sagittis adipiscing non at sem. Aliquam posuere, justo nec commodo interdum, magna erat euismod diam, eget egestas purus nisi a nisl. Nullam vestibulum magna quam, vitae vulputate mi lacinia quis. In in mauris at libero varius laoreet.
+            </p>
+            <p>
+                Nullam enim erat, mollis a dictum pellentesque, tincidunt ac ligula. Nulla vitae bibendum velit. In nec diam leo. Suspendisse iaculis enim ut nunc mattis, vitae laoreet lorem tempor. Sed non dapibus quam. Nulla vel dolor venenatis, commodo est eget, pretium metus. Ut ut arcu tortor. Cras sed sem a neque pharetra accumsan. Donec quis dolor tellus. Cras mollis tortor arcu, nec vestibulum nibh tincidunt sit amet.
+            </p>
+            <p>
+                Curabitur eros mauris, convallis id posuere eu, placerat quis felis. Maecenas sit amet purus blandit, euismod felis sed, pulvinar elit. Donec turpis enim, tincidunt et leo at, fermentum pharetra massa. In quis sapien est. Praesent condimentum ultricies magna, non eleifend ligula ultricies ac. Donec ultrices dui pellentesque congue feugiat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam ultricies interdum odio vel placerat.
+            </p>
+        </div>
+    </section>
     <!--debut footer-->
     <section class="l-footer-columns">
         <div class="row">
@@ -152,7 +173,8 @@
         <div class="row">
 
             <div class="copyright large-6 text-right columns">
-                &copy; 2014 &quot;Espace Numérique de Travail&quot;  All rights reserved.        </div>
+                &copy; 2014 &quot;Espace Numérique de Travail&quot; All rights reserved.
+            </div>
         </div>
     </footer>
 </div>
