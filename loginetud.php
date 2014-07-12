5976b7809d9dec0e15ba4e0b9e46ca3ce9869a3f
brunonc <brunonc@live.fr>
2014-07-12 18:53:33 +1100

    majinfo looks ok

--- loginetud.php
+++ loginetud.php
@@ -1,8 +1,5 @@
 <?php
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(-1);
-require($phpcas_path.'/CAS.php');
+require('./CAS.php');
 phpCAS::client(CAS_VERSION_2_0, $cas_host, $cas_port, $cas_context);
 phpCAS::setNoCasServerValidation();
 phpCAS::forceAuthentication();
