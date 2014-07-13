b1a9902fbe22f996fd79fec945e7cbd29a92bd9e
brunonc <brunonc@live.fr>
2014-07-13 13:44:13 +1100

    debugging prod

--- all.header.php
+++ all.header.php
@@ -7,9 +7,9 @@ if (!isset($_SESSION['connected'])) {
     // Load the settings from the central config file
     require_once 'config.php';
     // Load the CAS lib
-    /*require_once $phpcas_path . '/CAS.php';
+    require_once $phpcas_path . '/CAS.php';
     // Initialize phpCAS
-    phpCAS::client(CAS_VERSION_2_0, $cas_host, $cas_port, $cas_context);
+    /*phpCAS::client(CAS_VERSION_2_0, $cas_host, $cas_port, $cas_context);
     // For quick testing you can disable SSL validation of the CAS server.
     // THIS SETTING IS NOT RECOMMENDED FOR PRODUCTION.
     // VALIDATING THE CAS SERVER IS CRUCIAL TO THE SECURITY OF THE CAS PROTOCOL!
