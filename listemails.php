5fd27181bc5d3f604b7748ee28f29f86ac6562b2
Numerinico <nicolas.brengard@gmail.com>
2014-08-16 16:58:12 +1100

    update mail

--- listemails.php
+++ listemails.php
@@ -1,42 +1,43 @@
 <?php
 include('all.header.php');
+include('logincheck.php');
 if ($_SESSION['connected'] !== "admin") {
     header('Location : /');
     die();
 }
+$mysqli = new mysqli($sqlserver, $sqlid, $sqlpwd, $sqldb);
 if (isset($_GET['idMail'])) {
-    $query = "DELETE FROM Mail WHERE idMail='$_GET[idMail])'";
-    // Exécution de la requète
-    $result = mysqli_query($dblink, $query) or die("Erreur lors de la requète SQL: " . mysqli_error($dblink));
+    $stmt = $mysqli->prepare('DELETE FROM Mail WHERE idMail=?');
+    $stmt->bind_param('s', $_GET['idMail']);
+    $stmt->execute();
+    $stmt->close();
 }
 ?>
-    <h1>Liste des E-mails envoyés via la plateforme</h1>
-    <div id="col">
-        <h3>Recherche</h3>
+    <div class="row panel">
+        <div class="large-12 columns">
+            <h3>Recherche</h3>
 
-        <form action="listemails" method="GET" id="recherche">
-            <input placeholder="Mots-clés" type="text" name="champ_rech" id="champ_rech" maxlength="100" value="<?php if (isset($_GET['champ_rech'])) echo $_GET['champ_rech']; ?>"/>
-            <button id="recherche" type="submit">Rechercher</button>
-        </form>
+            <form action="listemails" method="GET" id="recherche">
+                <input placeholder="Mots-clés" type="text" name="champ_rech" id="champ_rech" maxlength="100" value="<?php if (isset($_GET['champ_rech'])) echo $_GET['champ_rech']; ?>"/>
+
+                <div class="row">
+                    <div class="large-centered large-6 columns">
+                        <input class="large button expand" id="envoyer" type="submit" value="Rechercher"/>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
-    <div class="cleaner h20"></div>
-    <h4>Résultats</h4>
 <?php
-$mysqli = new mysqli($sqlserver, $sqlid, $sqlpwd, $sqldb);
-if (!($stmt = $mysqli->prepare('SELECT idMail, destinataireMail, expediteurMail, sujetMail, dateEnvoiMail, messageMail FROM Mail WHERE destinataireMail
-                                LIKE ?  OR sujetMail LIKE ? OR messageMail LIKE ? OR expediteurMail LIKE ? ORDER BY dateEnvoiMail DESC'))
-) {
-    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
-}
+$stmt = $mysqli->prepare('SELECT idMail, destinataireMail, expediteurMail, sujetMail, dateEnvoiMail, messageMail FROM Mail WHERE destinataireMail
+                                LIKE ?  OR sujetMail LIKE ? OR messageMail LIKE ? OR expediteurMail LIKE ? ORDER BY dateEnvoiMail DESC');
 if (isset($_GET['champ_rech'])) {
     $search = "%" . $_GET['champ_rech'] . "%";
 } else {
     $search = "%%";
 }
 $stmt->bind_param('ssss', $search, $search, $search, $search);
-if (!($stmt->execute())) {
-    echo "Execute failed: (" . $mysqli->errno . ") " . $mysqli->error;
-}
+$stmt->execute();
 $stmt->bind_result($idMail, $destinataireMail, $expediteurMail, $sujetMail, $dateEnvoiMail, $messageMail);
 $stmt->store_result();
 if ($stmt->num_rows > 0) {
@@ -46,8 +47,8 @@ if ($stmt->num_rows > 0) {
             <table style="width: 100%">
                 <thead>
                 <tr>
-                    <th>Destinataire</th>
                     <th>Expediteur</th>
+                    <th>Destinataire</th>
                     <th>Sujet</th>
                     <th>Date</th>
                 </tr>
@@ -55,9 +56,9 @@ if ($stmt->num_rows > 0) {
                 <tbody>
                 <?php
                 while ($stmt->fetch()) {
-                    echo '<tr onclick="document.location.href=\'listemails?id='.$idMail.'\'"> ';
-                    echo '<td>' . $destinataireMail . '</td><td>' . $expediteurMail . '</td><td>' . $sujetMail . '</td>';
-                    echo '<td>' . strftime("%#d %B %Y à %Hh%M",strtotime($dateEnvoiMail)) . '</td>';
+                    echo '<tr onclick="document.location.href=\'listemails?id=' . $idMail . '\'"> ';
+                    echo '<td>' . $expediteurMail . '</td><td>' . $destinataireMail . '</td><td>' . $sujetMail . '</td>';
+                    echo '<td>' . strftime("%#d %B %Y à %Hh%M", strtotime($dateEnvoiMail)) . '</td>';
                     echo '</tr>';
                 }
                 ?>
@@ -65,7 +66,7 @@ if ($stmt->num_rows > 0) {
             </table>
         </div>
     </div>
-<?php
+    <?php
     $stmt->close();
 } else {
     ?>
@@ -77,32 +78,66 @@ if ($stmt->num_rows > 0) {
 <?php
 }
 if (isset($_GET['id'])) {
-
-    echo "<h4>Détails</h4>";
-    echo '<a href="listemails?idMail='.$_GET['id'].'"><button class="float_r" onclick="return confirm(\'Êtes-vous sur de vouloir supprimer définitivement ce message?\');">Supprimer le message</button></a>';
-    $query = "SELECT * FROM Mail WHERE idMail='$_GET[id]'";
-    // Exécution de la requète
-    $result = mysqli_query($dblink, $query) or die("Erreur lors de la requète SQL: ".mysqli_error($dblink));
-    // Affichage des colones
-    $data = mysqli_fetch_assoc($result);
-    echo '<div class="col_13 float_l">';
-    echo "<h5>Mail de </h5>".$data['expediteurMail'];
-    echo '<div class="cleaner h10"></div>';
-    echo '<a href="majinfo?idEtud='.$data['idEtud'].'">Profile</a>';
-    echo '<div class="cleaner h10"></div>';
-    echo "<h5>Envoyé le</h5>".utf8_encode (strftime("%#d %B %Y &agrave; %Hh%M",strtotime($data['dateEnvoiMail'])));
-    echo '<div class="cleaner h10"></div>';
-    echo "<h5>Pièce jointe:</h5>";
-    if ($data[cvMail] == "0") {
-        echo "aucune";
-    } else {
-        echo '<a href="'.$data['cvMail'].'" target="_blank">CV</a>';
-    }
-    echo '</div><div class="col_23 float_r">';
-    echo "<h5>Sujet</h5>".$data['sujetMail'];
-    echo '<div class="cleaner h10"></div>';
-    echo "<h5>Message</h5>".$data['messageMail'];
-    echo "</div>";
+    $stmt = $mysqli->prepare('SELECT idMail, destinataireMail, expediteurMail, sujetMail, dateEnvoiMail, messageMail, cvMail, idEtud FROM Mail WHERE idMail=?');
+    $stmt->bind_param('i', $_GET['id']);
+    $stmt->execute();
+    $stmt->bind_result($idMail, $destinataireMail, $expediteurMail, $sujetMail, $dateEnvoiMail, $messageMail, $cvMail, $idEtud);
+    $stmt->fetch();
+    ?>
+    <div class="row">
+        <div class="large-8 large-centered panel columns">
+            <div class="row">
+                <div class="large-6 columns">
+                    <h4>Détails</h4>
+                </div>
+                <div class="large-6 columns text-right">
+                    <a href="majinfo?idEtud=<?php echo $idEtud; ?>" class="button">Voir le profil</a>
+                    <a href="listemails?idMail=<?php echo $_GET['id']; ?>" class="button" onclick="return confirm('Êtes-vous sur de vouloir supprimer définitivement ce message?')">Supprimer le
+                        mail</a>
+                </div>
+            </div>
+            <div class="row collapse">
+                <div class="small-4 columns">
+                    <span class="prefix">Mail de </span>
+                </div>
+                <div class="small-8 columns">
+                    <input type="text" value="<?php echo $expediteurMail; ?>" disabled>
+                </div>
+            </div>
+            <div class="row collapse">
+                <div class="small-4 columns">
+                    <span class="prefix">Envoyé le </span>
+                </div>
+                <div class="small-8 columns">
+                    <input type="text" value="<?php echo strftime("%#d %B %Y &agrave; %Hh%M", strtotime($dateEnvoiMail)); ?>" disabled>
+                </div>
+            </div>
+            <div class="row collapse">
+                <div class="small-4 columns">
+                    <span class="prefix">Objet </span>
+                </div>
+                <div class="small-8 columns">
+                    <input type="text" value="<?php echo $sujetMail; ?>" disabled>
+                </div>
+            </div>
+            <div class="row">
+                <div class="large-12 large-centered text-right columns">
+                    <?php if ($cvMail == "") {
+                        echo '<a class="button">Pas de pièce jointe</a>';
+                    } else {
+                        echo '<a href="' . $cvMail . '" target="_blank" class="button">Voir la pièce jointe</a>';
+                    } ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="large-10 large-centered columns">
+                    <?php echo $messageMail; ?>
+                </div>
+            </div>
+            <br>
+        </div>
+    </div>
+<?php
+    $stmt->close();
 }
-
 include('all.footer.php');
