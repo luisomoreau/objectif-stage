0850ff2dd2d0f320c42928fb075fb64fd62d7566
brunonc <brunonc@live.fr>
2014-08-17 17:18:01 +1100

    Page "Mes stages"

--- messtages.php
+++ messtages.php
@@ -1,36 +1,47 @@
 <?php
-include('nontraite.php');
 include('all.header.php');
 // Chargement des paramètres de la DB
       
-$idEnt = $_SESSION[idEnt];
-// Requète SQL
-$query = "SELECT nomStage, sujetStage, dateDebutStage, dureeStage, idStage FROM stages WHERE idEnt='$idEnt'";
-// Exécution de la requète pour les colones
-$result = mysqli_query($dblink, $query) or die("Erreur lors de la requète SQL: ".mysqli_error($dblink));
-// Affichage des colones
-$data = mysqli_fetch_assoc($result);
+$idEnt = $_SESSION['id'];
+$mysqli = new mysqli($sqlserver,$sqlid,$sqlpwd,$sqldb);
+if (!($stmt = $mysqli->prepare('SELECT nomStage, sujetStage, dateDebutStage, dureeStage, idStage FROM stages WHERE idEnt=?'))) {
+    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+}
+$stmt->bind_param('s', $idEnt);
+if (!($stmt->execute())) {
+    echo "Execute failed: (" . $mysqli->errno . ") " . $mysqli->error;
+}
+$stmt->bind_result($nomStage, $sujetStage, $dateDebutStage, $dureeStage, $idStage);
+$stmt->store_result();
 ?>
-<h3>Mes stages</h3>
+<div class="row">
+    <div class="small-12">
+        <h3>Mes stages</h3>
 <?php
-if ($data == NULL) {
-    echo "Vous n'avez pas de stages.";
-} else {
-    echo "<table>";
-    echo "<tr><th>Nom du stage</th><th>Sujet</th><th>Début du stage</th><th>Durée du stage</th><th>Options</th></tr>";
-    // Exécution de la requète pour les valeures
-    $result = mysqli_query($dblink, $query) or die("Erreur lors de la requète SQL: ".mysqli_error($dblink));
+if ($stmt->num_rows > 0) {
+    echo '<table style="width: 100%">';
+    echo "<thead><tr><th>Nom du stage</th><th>Sujet</th><th>Début du stage</th><th>Durée du stage</th><th>Options</th></tr></thead><tbody>";
     // Remplissage du tableau
-    while($data = mysqli_fetch_assoc($result)) {
+    while ($stmt->fetch()) {
         echo "<tr>";
-        echo '<tr onclick="document.location.href=\'infostage?id='.$data[idStage].'\'">';
-        echo "<td>$data[nomStage]</td><td>$data[sujetStage]</td><td>".utf8_encode(strftime("%#d %B %Y",strtotime($data[dateDebutStage])))."</td><td>$data[dureeStage] jours</td>";
-        echo '<td><a href="majstage?id='.$data[idStage].'">Modifier</a></td>';
+        echo '<tr onclick="document.location.href=\'infostage?id='.$idStage.'\'">';
+        echo "<td>$nomStage</td><td>$sujetStage</td><td>".utf8_encode(strftime("%#d %B %Y",strtotime($dateDebutStage)))."</td><td>$dureeStage jours</td>";
+        echo '<td><a href="majstage?id='.$idStage.'">Modifier</a></td>';
         echo "</tr>";
     }
     // On ferme le tableau
-    echo "</table>";
-}
-
+    echo "</tbody></table>";
+} else {
+    ?>
+    <div class="row">
+        <div class="small-12 columns text-center">
+            <h3>Vous n'avez pas de stages</h3>
+        </div>
+    </div>
+<?php
+} ?>
+    </div>
+</div>
+<?php
 include ('all.footer.php');
 ?>
