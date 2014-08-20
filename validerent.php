741aa0c49bd20a39aeaede12dd8cb51e4465a222
Numerinico <nicolas.brengard@gmail.com>
2014-08-20 21:29:55 +1100

    fix validation

--- validerent.php
+++ validerent.php
@@ -6,25 +6,24 @@ if ($_SESSION['connected'] !== "admin") {
 } else {
     $mysqli = new mysqli($sqlserver, $sqlid, $sqlpwd, $sqldb);
     if (isset($_GET['idEnt'])) {
-        $stmt = $mysqli->prepare('UPDATE entreprises SET valideEnt=1 WHERE idEnt = ?');
+        $stmt = $mysqli->prepare('SELECT mailEnt, nomEnt, valideEnt FROM entreprises WHERE idEnt=?');
         $stmt->bind_param('i', $_GET['idEnt']);
         $stmt->execute();
-
-        if (!($stmt = $mysqli->prepare('SELECT mailEnt FROM entreprises WHERE idEnt=?'))) {
-            echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
-        }
-        $stmt->bind_param('i', $_GET['idEnt']);
-        if (!($stmt->execute())) {
-            echo "Execute failed: (" . $mysqli->errno . ") " . $mysqli->error;
-        }
-        $stmt->bind_result($mailEnt);
+        $stmt->bind_result($mailEnt, $nomEnt, $valideEnt);
         $stmt->fetch();
         $stmt->close();
 
-        email($mail_account, $mail_pwd, $mailEnt, 'Objectif stage : Votre entreprise a été validée',
-            'Bonjour,<br><br>Votre entreprise a bien été validée vous pouvez à présent vous connecter.<br><br><a href="https://stages.univ-nc.nc/">Lien vers la plateforme de stages</a>',
-            'stages@univ-nc.nc',
-            'Plateforme Objectif stage', '0');
+        if ($valideEnt == 0) {
+            $stmt = $mysqli->prepare('UPDATE entreprises SET valideEnt=1 WHERE idEnt = ?');
+            $stmt->bind_param('i', $_GET['idEnt']);
+            $stmt->execute();
+            $stmt->close();
+
+            email($mail_account, $mail_pwd, $mailEnt, "Objectif stage : Votre entreprise $nomEnt a été validée",
+                "Bonjour,<br><br>Votre entreprise $nomEnt a bien été validée vous pouvez à présent vous connecter.<br><br><a href=\"https://stages.univ-nc.nc/\">Lien vers la plateforme de stages</a>",
+                'stages@univ-nc.nc',
+                'Plateforme Objectif stage', '0');
+        }
     }
 
     $stmt = $mysqli->prepare('SELECT idEnt, nomEnt, telEnt, adresseEnt FROM entreprises WHERE valideEnt = 0');
@@ -32,41 +31,42 @@ if ($_SESSION['connected'] !== "admin") {
     $stmt->bind_result($idEnt, $nomEnt, $telEnt, $adresseEnt);
     $stmt->store_result();
     if ($stmt->num_rows > 0) {
-    ?>
-    <div class="row">
-        <div class="small-12">
-            <h4>Liste des entreprises non validées </h4>
-            <table style="width: 100%">
-                <thead>
-                <tr>
-                    <th>Nom de l'entreprise</th>
-                    <th>Téléphone</th>
-                    <th>Adresse</th>
-                    <th>Options</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php
-                while ($stmt->fetch()) {
-                    echo '<tr onclick="document.location.href=\'infoentreprise?id=' . $idEnt . '\'">';
-                    echo '<td>' . $nomEnt . '</td><td>' . $telEnt . '</td><td>' . $adresseEnt . '</td>';
-                    echo '<td><a href="infoentreprise?id=' . $idEnt . '">Plus d\'infos</a></td>';
-                    echo '</tr>';
-                }
-                ?>
-                </tbody>
-            </table>
+        ?>
+        <div class="row">
+            <div class="small-12">
+                <h4>Liste des entreprises non validées </h4>
+                <table style="width: 100%">
+                    <thead>
+                    <tr>
+                        <th>Nom de l'entreprise</th>
+                        <th>Téléphone</th>
+                        <th>Adresse</th>
+                        <th>Options</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    while ($stmt->fetch()) {
+                        echo '<tr onclick="document.location.href=\'infoentreprise?id=' . $idEnt . '\'">';
+                        echo '<td>' . $nomEnt . '</td><td>' . $telEnt . '</td><td>' . $adresseEnt . '</td>';
+                        echo '<td><a href="infoentreprise?id=' . $idEnt . '">Plus d\'infos</a></td>';
+                        echo '</tr>';
+                    }
+                    ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
-<?php
+        <?php
+        $stmt->close();
     } else {
-?>
+        ?>
         <div class="row">
             <div class="small-12">
                 <h4>Pas d'entreprises à valider</h4>
             </div>
         </div>
-<?php
+    <?php
     }
 }
 include('all.footer.php');
