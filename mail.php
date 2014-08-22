de2e3c8cf7d14eef530e8f4586c09e93b0340f72
brunonc <brunonc@live.fr>
2014-08-22 16:10:03 +1100

    fix mail

--- mail.php
+++ mail.php
@@ -51,10 +51,10 @@ if (isset($_POST['cv']) && isset($_POST['dest']) && isset($_POST['cible']) && is
     }
     if (!is_null($idEnt)) {//si le mail existe et correspon bien à la cible demandée (stage : réponse à une offre, ent : proposition volontaire)
         if (email($mail_account, $mail_pwd, $destinataire, $sujet, $message, $expediteur, $nom_expediteur, $piece_jointe)) {
-            $stmt = $mysqli->prepare('INSERT INTO mail (idEtud, idEnt, destinataireMail, sujetMail, messageMail, expediteurMail, cvMail)
+            $stmt = $mysqli->prepare('INSERT INTO mail (userEtud, idEnt, destinataireMail, sujetMail, messageMail, expediteurMail, cvMail)
                         VALUES (?,?,?,?,?,?,?)');
             echo "Execute failed: (" . $mysqli->errno . ") " . $mysqli->error;
-            $stmt->bind_param('iisssss', $_SESSION['idEtud'], $idEnt, $destinataire, $sujet, $message, $nom_expediteur, $piece_jointe);
+            $stmt->bind_param('sisssss', $_SESSION['identifiant'], $idEnt, $destinataire, $sujet, $message, $nom_expediteur, $piece_jointe);
             echo "Execute failed: (" . $mysqli->errno . ") " . $mysqli->error;
             $stmt->execute();
             echo "Execute failed: (" . $mysqli->errno . ") " . $mysqli->error;
