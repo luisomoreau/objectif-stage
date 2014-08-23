cdf6d0e7b091afb374b30489f35be75925e99ab3
brunonc <brunonc@live.fr>
2014-08-23 17:43:33 +1100

    Last fixes

--- mail.php
+++ mail.php
@@ -1,9 +1,6 @@
 <?php
 include('all.header.php');
 include('logincheck.php');
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(-1);
 $user = getInfos();
 if ($user == null) {
     header('Location: ./');
